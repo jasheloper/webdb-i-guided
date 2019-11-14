1efd3379449c3e4f7070ec903dc16c1f4f70acdf
Jashele T <jashele@Jasheles-Air.domain>
2019-11-13 17:36:49 -0700

    working on CRUD operations getting info from DB

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  db('posts').select('*')
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+       res.status(500).json({message: 'problem with the database.'})
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+   const {id} = req.params;
+   db('posts').select('*').where({id})
+   .then(post => {
+      res.status(200).json(post);
+   })
+   .catch(err => {
+      res.status(500).json({ message: "problem with database"})
+   })
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.put("/:id", (req, res) => {});
 
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
